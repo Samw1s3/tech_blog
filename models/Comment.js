cc51adea0824c29c7c8101d14e11406587daed24
Sam Knopper <samanthaknopper@hotmail.com>
2022-05-22 17:18:25 +0800

    models created

--- models/Comment.js
+++ models/Comment.js
@@ -2,27 +2,31 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 const User = require('./user');
+const Post = require('./Post');
 
-class Post extends Model {
+class Comment extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-Post.init(
+Comment.init(
   {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    content: {
+    body: {
       type: DataTypes.TEXT,
       allowNull: false,
-      },
+    },
     user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        },
+      },
+    post_id: {
       type: DataTypes.INTEGER,
       references: {
-          model: User,
+          model: Post,
           key: 'id'
       },
     },
@@ -32,8 +36,8 @@ Post.init(
     timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: 'Post',
+    modelName: 'Comment',
   }
 );
 
-module.exports = Post;
+module.exports = Comment;
