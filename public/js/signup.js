136632978a86bc82b60855755d2ab0ae8d8d5a02
Sam Knopper <samanthaknopper@hotmail.com>
2022-05-29 21:36:31 +0800

    signup not working

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,29 @@
-// async function signupFormHandler(event) {
-//     event.preventDefault();
+const signUpFormHandler = async (event) => {
+    event.preventDefault();
   
-//     const user_name = document.querySelector('#user_name-signup').value.trim();
-//     const email = document.querySelector('#email-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
-      
-//     if (user_name && email && password) {
-//       const response = await fetch('/api/users', {
-//         method: 'post',
-//         body: JSON.stringify({ user_name, email, password }),
-//         headers: { 'Content-Type': 'application/json' }
-//       });
+    const user_name = document.querySelector("#user_user_user_name-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
   
-//       if (response.ok) {
-//         document.location.replace('/dashboard');
-//       } else {
-//         alert(response.statusText);
-//       }
-//     }
-//   }
+    if (user_name && email && password) {
+      // create a post request with the details entered
+      const response = await fetch("/api/users/", {
+        method: "POST",
+        body: JSON.stringify({ user_name, email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
   
-// document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+      if (response.ok) {
+        // redirect to homepage
+        document.location.replace("/home");
+      } else {
+        //  alert user if request has failed
+        alert("Failed to sign in");
+      }
+    }
+  };
+  
+  // event listener for the submit button
+  document
+    .querySelector(".signup-form")
+    .addEventListener("submit", signUpFormHandler);
