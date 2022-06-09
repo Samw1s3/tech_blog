334ee778d9d56a3c0194d59b6a79ce0d45f7334a
Sam Knopper <samanthaknopper@hotmail.com>
2022-06-09 22:25:55 +0800

    address name change

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User  = require('./user');
-const Post = require('./post');
-const Comment = require('./comment');
+const Post = require('./Post');
+const Comment = require('./Pomment');
 
 Post.belongsTo(User, {
   foreignKey: 'userId',
